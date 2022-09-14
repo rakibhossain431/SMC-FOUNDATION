9bd270152884dee12f90661f02fc31de3524d58f
Rakib Hossain <rakibjp431@gmail.com>
2022-09-14 16:22:11 +0600

    Add Home page Banner

---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../../Shared/Banner/Banner'
+import SmcRevolution from './SmcRevolution/SmcRevolution'
+
+function Home() {
+  return (
+    <>
+        <Banner/>
+        <SmcRevolution/>
+    </>
+  )
+}
+
+export default Home 
---
+++ src/Components/Pages/Home/SmcRevolution/SmcRevolution.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SmcRevolution() {
+  return (
+    <section className='revolution'>
+      <div className='container'>
+        <h2>How SMC is Revolutionary </h2>
+        <div className='row'>
+            <div className="card">
+              <img src="" alt="" />
+            </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SmcRevolution
