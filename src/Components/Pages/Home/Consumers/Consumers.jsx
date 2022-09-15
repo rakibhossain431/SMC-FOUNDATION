b0f21a1f5342695c0a3baa3c3fe44b667c775f07
tanvinpavel <tanvinpavel123@gmail.com>
2022-09-15 11:23:39 +0600

    Css File Structure Add

---
+++ src/Components/Pages/Home/Consumers/Consumers.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Consumers = () => {
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-md-6"></div>
+                <div className="col-md-6"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Consumers;
