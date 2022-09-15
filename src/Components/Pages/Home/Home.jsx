8ffbb3bbc20abde2cdd14fb3df58381aaff615e0
Rakib Hossain <rakibjp431@gmail.com>
2022-09-15 11:28:00 +0600

    Add Header

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../Shared/Banner/Banner'
+import Header from '../../Shared/Banner/Header/Header'
 import SmcRevolution from './SmcRevolution/SmcRevolution'
 
 function Home() {
   return (
     <>
+        <Header/>
         <Banner/>
         <SmcRevolution/>
     </>
--- src/Components/Pages/Home/SmcRevolution/SmcRevolution.jsx
+++ src/Components/Pages/Home/SmcRevolution/SmcRevolution.jsx
@@ -2,15 +2,34 @@ import React from 'react'
 
 function SmcRevolution() {
   return (
-    <section className='revolution'>
-      <div className='container'>
-        <h2>How SMC is Revolutionary </h2>
-        <div className='row'>
-            <div className="card">
-              <img src="" alt="" />
+        <section className=' px-2 py-5'>
+        <div className="container" >
+            <h2 className="r-title pb-3">How SMC is Revolutionary  </h2>
+            <div className='revolution d-flex'>
+                <div className='r-box'>
+                  <img src="/images/Revolution/secure.png" alt="" />
+                  <h4>Secure </h4>
+                    <p className='r-text mt-0'>The SMC Blockchain is as secure as bitcoin</p>         
+                </div>
+                <div className='r-box'> 
+                  <img src="/images/Revolution/Fast.png" alt="" />
+                  <h4>Fast</h4>
+                    <p className='r-text mt-0'>The SMC Blockchain is as secure as bitcoin</p>
+                </div>
+                <div className='r-box'>
+                  <img src="/images/Revolution/Scenabol.png" alt="" />
+                  <h4>Scalable</h4>
+                    <p className='r-text mt-0'>Unlike any other, SMC Blockchain is infinitely scalable </p> 
+                </div>
+                <div className='r-box'>
+                  <img src="/images/Revolution/Reliable.png" alt="" />
+                  <h4>Reliable</h4>
+                    <p className='r-text mt-0'>Network failures are impossible in the SMC Blockchain </p>
+                </div>
+
             </div>
         </div>
-      </div>
+
     </section>
   )
 }
